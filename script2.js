e2426b599a114a3f43f5f4d43f430cac17d75c81
Aman Ahamed <amanahamed04gmail.com>
2024-08-21 20:03:52 +0530

    B.E Series - async

--- script2.js
+++ script2.js
@@ -18,4 +18,13 @@ function abcd() {
 // console.log(abcd());
 
 // async js coding
+// the code which is sync by nature -> send them to main stack and run
+// if you get code which is async -> send them to side stack and process
+// after sync code ran in main stack -> bring the async code from side stack to main stack and then run that code
 
+async function abcd() {
+  var blob = await fetch("https://randomuser.me/api");
+  var ans = await blob.json();
+  console.log(ans.results[0].name);
+}
+abcd();
